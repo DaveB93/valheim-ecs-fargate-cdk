37e92c9725f19836752c0e33a21ef33a6d5c1374
DaveB93 <dave@DESKTOP-E5E4138.localdomain>
2021-03-25 15:53:13 -0700

    CDK stack now includes optional lambda functions for monitoring server status, and starting and stopping the server

--- bin/valheim-server-aws-cdk.ts
+++ bin/valheim-server-aws-cdk.ts
@@ -2,6 +2,28 @@
 import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
 import { ValheimServerAwsCdkStack } from "../lib/valheim-server-aws-cdk-stack";
+import { LambdaEcsFargateUpdownstatusStack } from '../lib/lambda-ecs-fargate-updownstatus-stack';
+
+class ValheimServerProps {
+    addAppGatewayStartStopStatus: boolean;
+}
+
+class ValheimServer extends cdk.Construct {
+    constructor(scope: cdk.Construct, id: string, props?: ValheimServerProps) {
+        super(scope, id);
+        var ecsStack = new ValheimServerAwsCdkStack(app, "ValheimServerAwsCdkStack");
+        if( props?.addAppGatewayStartStopStatus )
+        {
+            var lambdaStack = new LambdaEcsFargateUpdownstatusStack(app, 'LambdaEcsFargateUpdownstatusStack', {
+                serviceArn: cdk.Fn.importValue("fargateServiceName"),
+                clusterArn: cdk.Fn.importValue("fargateClusterName"),
+                startStopPassword: "changeme",
+            });
+            lambdaStack.addDependency(ecsStack);
+        }
+    }
+}
 
 const app = new cdk.App();
-new ValheimServerAwsCdkStack(app, "ValheimServerAwsCdkStack");
+new ValheimServer(app, "ValheimServer", {addAppGatewayStartStopStatus: true});
+app.synth();
